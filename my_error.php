073476cf5c1446940491ce6aa4b4c80a46592541
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-01 12:40:50 +0200

    lvl 3 opdr 1

---
+++ my_error.php
@@ -0,0 +1,18 @@
+<?php 
+    
+    function countDown() {
+        $number = 12;
+        try {
+            throw new Exception('foo');
+        } catch(Exception $e) {
+                if ($number == 0 && $number < 11) {
+                return "er is iets goed gegaan";
+                }else{
+                    return "er is iets fout gegaan";
+                }
+        }
+    }
+    echo countDown()
+?>
+    
+  
